acb02e2d6fba2e592e13ced0372d061d349e4453
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-02-16 12:11:42 +0530

    Create shapeSetgame view model

---
+++ Set/ShapeSetCardStyle.swift
@@ -0,0 +1,57 @@
+//
+//  ShapeSetCardStyle.swift
+//  Set
+//
+//  Created by Nikhil Henry on 16/02/22.
+//
+
+import Foundation
+
+struct ShapeSetGameCardStyle{
+
+enum cardNumbers:Int,CaseIterable {
+  case one = 1
+  case two = 2
+  case three = 3
+}
+
+enum cardColors:CaseIterable{
+  case green
+  case blue
+  case red
+}
+
+enum cardContents:CaseIterable{
+  case rectange
+  case circle
+  case diamond
+}
+
+enum cardShadings:CaseIterable {
+  case striped
+  case solid
+  case open
+}
+
+struct CardStyle:SetCardStyle{
+  var contentNumber: cardNumbers
+  var cardContent: cardContents
+  var cardShading: cardShadings
+  var cardColor: cardColors
+}
+
+func generateUniqueCardStlyes()->[CardStyle]{
+  var cardStlyes:[CardStyle] = []
+  for cardNumber in cardNumbers.allCases{
+    for cardContent in cardContents.allCases{
+      for cardShading in cardShadings.allCases{
+        for cardColor in cardColors.allCases{
+          cardStlyes
+            .append(CardStyle(contentNumber: cardNumber, cardContent: cardContent, cardShading: cardShading, cardColor: cardColor))
+        }
+      }
+    }
+  }
+  return cardStlyes
+}
+}
---
+++ Set/ShapeSetGame.swift
@@ -0,0 +1,25 @@
+//
+//  ShapeSetGame.swift
+//  Set
+//
+//  Created by Nikhil Henry on 16/02/22.
+//
+
+import SwiftUI
+
+class ShapeSetGame:ObservableObject{
+  
+  static private let cardStyles = ShapeSetGameCardStyle()
+  
+  typealias CardStlyes = ShapeSetGameCardStyle.CardStyle
+  
+  typealias Card = SetGame<ShapeSetGameCardStyle.CardStyle>.Card
+  
+  @Published private var model = SetGame<CardStlyes>(createUniqueCardStyles: cardStyles.generateUniqueCardStlyes)
+  
+  var cards: [Card]{
+    return model.cards
+  }
+  
+  
+}
