9e64bebe97a5340b08aaea6aeef7e4bc5b21c6f8
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-02-16 12:11:56 +0530

    Implement view model in view

--- Set/ContentView.swift
+++ Set/ContentView.swift
@@ -8,14 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  @ObservedObject var viewModel:ShapeSetGame
+  
+  var body: some View {
+    VStack{
+      Text("Memorize!").font(.largeTitle).foregroundColor(.black)
+      ScrollView{
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]){
+          ForEach(viewModel.cards){ card in
+            CardView(card: card)
+              .aspectRatio(2/3, contentMode: .fit)
+          }
+        }
+      }
+      .padding(.horizontal)
+      .foregroundColor(.red)
     }
+  }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+struct ThemeView: View{
+  var themeIcon: String
+  var themeTitle: String
+  var themeAction: () -> Void
+  var body: some View{
+    Button(action:{
+      themeAction()
+    },label:{
+      VStack{
+        Image(systemName: themeIcon).font(.largeTitle)
+        Text(themeTitle)
+          .font(.caption)
+      }
+      .foregroundColor(.blue)
+    })
+  }
 }
+
+
+struct CardView:View{
+  let card:ShapeSetGame.Card
+  var body: some View{
+    Text("card")
+  }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    let game = ShapeSetGame()
+    ContentView(viewModel:game)
+  }
+}
+
--- Set/SetApp.swift
+++ Set/SetApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct SetApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+          let game = ShapeSetGame()
+          ContentView(viewModel:game)
         }
     }
 }
