bf7936ed4a69061909cf0f183da8168f1c357938
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-02-16 10:01:24 +0530

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Nikhil Henry on 16/02/22.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
