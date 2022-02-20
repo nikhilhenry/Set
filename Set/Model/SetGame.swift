0fb1d173b4a49be6a25709d75c446aab7aadf129
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-02-20 09:43:41 +0530

    refactor required task 8(b)

--- Set/Model/SetGame.swift
+++ Set/Model/SetGame.swift
@@ -17,10 +17,10 @@ struct SetGame<CardStyle:SetCardStyle>{
   private var setFound = false
   
   init(createUniqueCardStyles:()->[CardStyle]){
-//  create cards for deck
+    //  create cards for deck
     createUniqueCardStyles().enumerated()
       .forEach{deck.append(Card(id:$0,cardStyle:$1))}
-//  deal 12 cards from deck
+    //  deal 12 cards from deck
     deck.first(12).indices.forEach { cards.append(deck[$0]); deck[$0].isDealt = true }
   }
   
@@ -30,21 +30,18 @@ struct SetGame<CardStyle:SetCardStyle>{
     
     if setFound{
       if (deck.filter{!$0.isDealt}.count > 0){
-      selectedCardIndices.forEach{ index in
-        let card = deck.filter{!$0.isDealt}[0]
-//      deal that card
-        deck[card.id].isDealt = true
-        cards[index] = card
+        selectedCardIndices.forEach{ index in
+          let card = deck.filter{!$0.isDealt}[0]
+          // deal that card
+          deck[card.id].isDealt = true
+          cards[index] = card
         }
       }else{
-//      remove those three cards
-        let selectedCards = selectedCardIndices.map{cards[$0]}
-        selectedCards.forEach{ card in
-          if let cardIndex = cards.firstIndex(where: {$0.id == card.id}){
+        // remove those three cards
+        selectedCardIndices.map{cards[$0]}
+        .forEach{ card in
+          guard let cardIndex = cards.firstIndex(where: {$0.id == card.id}) else {return}
           cards.remove(at: cardIndex)
-          }else{
-            print("card not found")
-          }
         }
       }
       setFound = false
@@ -53,7 +50,7 @@ struct SetGame<CardStyle:SetCardStyle>{
     
     let choosenIndex = cards.firstIndex(where: {$0.id == card.id})!
     
-//  deselect the card if already chosen
+    //  deselect the card if already chosen
     if let selectedIndex = selectedCardIndices.firstIndex(of: choosenIndex){
       selectedCardIndices.remove(at: selectedIndex)
       return
@@ -85,7 +82,7 @@ struct SetGame<CardStyle:SetCardStyle>{
 extension Array{
   func first(_ tillIndex:Int) -> [Element]{
     let upto = tillIndex > self.count ? self.count : tillIndex
-      return Array(self[0..<upto])
+    return Array(self[0..<upto])
   }
 }
 
