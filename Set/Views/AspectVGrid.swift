57a6d0b87c70a54fa886291029bfb4e7f56c50bb
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-02-27 11:49:14 +0530

    add scroll support

--- Set/Views/AspectVGrid.swift
+++ Set/Views/AspectVGrid.swift
@@ -21,10 +21,13 @@ struct AspectVGrid<Item, ItemView>: View where ItemView: View, Item: Identifiabl
   var body: some View {
     GeometryReader { geometry in
       VStack {
-        let width: CGFloat = widthThatFits(itemCount: items.count, in: geometry.size, itemAspectRatio: aspectRatio)
-        LazyVGrid(columns: [adaptiveGridItem(width: width)], spacing: 0) {
-          ForEach(items) { item in
-            content(item).aspectRatio(aspectRatio, contentMode: .fit)
+        ScrollView {
+          let width: CGFloat = max(widthThatBestFits(itemCount: items.count, in: geometry.size, itemAspectRatio: aspectRatio), 80)
+          LazyVGrid(columns: [adaptiveGridItem(width: width)], spacing: 0) {
+            ForEach(items) {
+              content($0)
+                .aspectRatio(aspectRatio, contentMode: .fit)
+            }
           }
         }
         Spacer(minLength: 0)
@@ -38,7 +41,7 @@ struct AspectVGrid<Item, ItemView>: View where ItemView: View, Item: Identifiabl
     return gridItem
   }
   
-  private func widthThatFits(itemCount: Int, in size: CGSize, itemAspectRatio: CGFloat) -> CGFloat {
+  private func widthThatBestFits(itemCount: Int, in size: CGSize, itemAspectRatio: CGFloat) -> CGFloat {
     var columnCount = 1
     var rowCount = itemCount
     repeat {
@@ -48,7 +51,8 @@ struct AspectVGrid<Item, ItemView>: View where ItemView: View, Item: Identifiabl
         break
       }
       columnCount += 1
-      rowCount = (itemCount + (columnCount - 1)) / columnCount
+      rowCount = Int(ceil(Double(itemCount) / Double(columnCount)))
+      
     } while columnCount < itemCount
     if columnCount > itemCount {
       columnCount = itemCount
