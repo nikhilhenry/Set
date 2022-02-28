bb3c27251c83b7166a217ce6704b9964e7665d11
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-02-28 15:01:55 +0530

    capture state of discarded cards

--- Set/Views/GameView.swift
+++ Set/Views/GameView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct GameView: View {
   @ObservedObject var game: ShapeSetGame
-
+  
   var body: some View {
     VStack {
       Text("Set!").font(.largeTitle).foregroundColor(.black)
@@ -21,12 +21,40 @@ struct GameView: View {
       cardPiles
     }
   }
+  
+  //  private state used to track discarded cards
+  @State private var discarded = Set<Int>()
+  
+  //  marks the given card as discarded
+  private func discard(_ card: ShapeSetGame.Card){
+    discarded.insert(card.id)
+  }
+  
+  // returns if the given card has not yet been discarded
+  private func isDiscarded(_ card: ShapeSetGame.Card) -> Bool {
+    discarded.contains(card.id)
+  }
+  
+  // the view for discarded cards
+  var discardPile:some View{
+    ZStack {
+      ForEach(game.descardedCards.reversed()) {card in
+        if isDiscarded(card){
+          CardView(card: card).aspectRatio(2 / 3, contentMode: .fit)
+        }
+        else{
+          Color.clear
+        }
+      }
+    } .frame(width: 60, height: 90, alignment: .center)
+  }
+  
   var cardPiles: some View {
     HStack {
       cardPile(for: game.deck)
         .onTapGesture {game.dealNewCards()}
       Spacer()
-      cardPile(for: game.descardedCards)
+      discardPile
     }
     .padding(.horizontal)
   }
@@ -34,10 +62,10 @@ struct GameView: View {
     HStack {
       if game.deckCount > 0 {
         Button {game.dealNewCards()}label: {Text("Deal 3 More Cards")}
-          .buttonStyle(.borderedProminent)
+        .buttonStyle(.borderedProminent)
       }
-        Button {game.startNewGame()}label: {Text("New Game")}
-          .buttonStyle(.bordered)
+      Button {game.startNewGame()}label: {Text("New Game")}
+      .buttonStyle(.bordered)
     }
   }
   private func cardPile(for items: [ShapeSetGame.Card]) -> some View {
